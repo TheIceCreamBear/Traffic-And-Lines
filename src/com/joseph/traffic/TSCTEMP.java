ee9cd7da10cac07f6d7a1d4d5ed68d2edd1e6dd3
Joseph <kartingman25@gmail.com>
2020-07-24 01:48:52 -0400

    Unfinished camera testing, attempting to get zoom to cursor

--- src/com/joseph/traffic/TSCTEMP.java
+++ src/com/joseph/traffic/TSCTEMP.java
@@ -1,19 +1,57 @@
 package com.joseph.traffic;
 
+import java.awt.Point;
+import java.awt.event.MouseWheelEvent;
 import java.awt.geom.AffineTransform;
 
 import com.projecttriumph.engine.rendering.Camera;
+import com.projecttriumph.engine.rendering.ScreenManager;
 
 public class TSCTEMP extends Camera {
+	private static final double ZOOM_FACTOR = 0.1d;
+	private AffineTransform transform = new AffineTransform();
+	
 	@Override
-	public AffineTransform getCurrentTransform() {
-		AffineTransform at = new AffineTransform();
+	public synchronized AffineTransform getCurrentTransform() {
 		// shifts the center of the scaled screen to the center of the screen to create zooming from center
-		at.translate(-300 * (scale - 1), -200 * (scale - 1));
-		// zooms the screen 
-		at.scale(scale, scale);
+//		Point2D p = ScreenManager.getInstance().getMousePoint();
+//		try {
+//			p = ScreenManager.getInstance().getCamera().getCurrentTransform().inverseTransform(p, null);
+//		} catch (NoninvertibleTransformException ex) {
+//			ex.printStackTrace();
+//		}
+//		at.translate(-lastZoom.getX() * (scale - 1), -lastZoom.getY() * (scale - 1));
+//		// zooms the screen 
+//		at.scale(scale, scale);
 		// Translates to the off set position
-		at.translate(offsetX, offsetY);
-		return at;
+		synchronized (transform) {			
+			AffineTransform at = new AffineTransform(transform);
+			at.translate(offsetX, offsetY);
+			return at;
+		}
 	}
+
+	@Override
+	public synchronized void onMouseWheelEvent(MouseWheelEvent event) {
+//		super.onMouseWheelEvent(event);
+		
+		Point mouse = ScreenManager.getInstance().getMousePoint();
+		int rotation = event.getWheelRotation();
+		double scaleFactor = ZOOM_FACTOR * rotation * -1 + 1;
+		
+		synchronized (transform) {			
+			System.out.println("+=================================================================+");
+			transform.translate(-mouse.x * (scaleFactor - 1), -mouse.y * (scaleFactor - 1));
+			System.out.println(transform);
+			transform.scale(scaleFactor, scaleFactor);
+			System.out.println(transform);
+			// restores true center (0,0)
+//		transform.translate(mouse.x / scaleFactor, mouse.y / scaleFactor);
+			// over translates (when scale factor is 1.1, by 1.1 times)
+//		transform.translate(mouse.x, mouse.y);
+//		transform.translate(mouse.x, mouse.y);
+			System.out.println(transform);
+		}
+	}
+	
 }
